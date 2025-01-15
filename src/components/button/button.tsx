9d6680f563564402a99ecf54b2c3d3147671aace
dhee-chowdhury <dhee.chowdhury@gmail.com>
2025-01-15 19:44:52 +0600

    batman

---
+++ src/components/button/button.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from "react";
+
+import clsx from "clsx";
+
+
+import styles from './button.module.css'
+
+export type ButtonProps = ComponentProps<'button'> & {
+  variant: 'primary' | 'secondary' | 'destructive'
+}
+
+export const Button = ({variant,className, ...props}: ButtonProps) => {
+  const classes = clsx(styles.button, styles[variant], className)
+  return <button {...props} className={classes}/>;
+};
