8c0e58575ea38ba4cdbfe720a56bb596d609329f
dhee-chowdhury <dhee.chowdhury@gmail.com>
2025-01-16 04:09:01 +0600

    added theme

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,38 +1,68 @@
-import type {Meta, StoryObj} from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from "./button";
+import { Button } from './button';
 
 const meta = {
-    title: 'Button',
-    component: Button,
-    args: {
-        children: 'Button',
-        disabled: false,
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Button',
+    disabled: false,
+    size: 'medium',
+  },
+  argTypes: {
+    disabled: {
+      control: 'boolean',
     },
-    argTypes: {
-        disabled: {
-            control: 'boolean'
-        }
-    }
+    variant: {
+      control: 'select',
+    },
+  },
 } satisfies Meta;
-
 
 export default meta;
 
-type Story = StoryObj<typeof  Button>;
+type Story = StoryObj<typeof Button>;
 export const Primary: Story = {
-args: {
-    variant: 'primary'
-}
-}
+  args: {
+    variant: 'primary',
+  },
+};
 export const Secondary: Story = {
-args: {
-    variant: 'secondary'
-}
-} 
+  args: {
+    variant: 'secondary',
+  },
+};
 
 export const Destructive: Story = {
-    args: {
-        variant: 'destructive'
-    }
-}
+  args: {
+    variant: 'destructive',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+  },
+};
+export const Large: Story = {
+  args: {
+    size: 'large',
+  },
+};
+
+export const Dark: Story = {
+  parameters: {
+    themes: {
+      themeOverride: 'dark',
+    },
+  },
+};
+
+export const Mobile: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'mobile1',
+    },
+  },
+};
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,15 +1,20 @@
-import { ComponentProps } from "react";
+import { ComponentProps } from 'react';
 
-import clsx from "clsx";
+import clsx from 'clsx';
 
-
-import styles from './button.module.css'
+import styles from './button.module.css';
 
 export type ButtonProps = ComponentProps<'button'> & {
-  variant: 'primary' | 'secondary' | 'destructive'
-}
+  variant?: 'primary' | 'secondary' | 'destructive';
+  size?: 'small' | 'medium' | 'large';
+};
 
-export const Button = ({variant,className, ...props}: ButtonProps) => {
-  const classes = clsx(styles.button, styles[variant], className)
-  return <button {...props} className={classes}/>;
+export const Button = ({
+  variant = 'primary',
+  size = 'medium',
+  className,
+  ...props
+}: ButtonProps) => {
+  const classes = clsx(styles.button, styles[variant], styles[size], className);
+  return <button {...props} className={classes} />;
 };
